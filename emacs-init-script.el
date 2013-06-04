9f665a014a78e8c54f84f0cfe744a886ef10ae13
David Diederich <divido@psu.edu>
2013-06-04 17:31:14 -0400

    Added customization to change the default font/size
    Set the default font size to be 8 pt Courier New.

--- emacs-init-script.el
+++ emacs-init-script.el
@@ -109,11 +109,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#002b36" :foreground "#839496" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 80 :width normal :foundry "raster" :family "Courier New"))))
  '(semantic-decoration-on-unknown-includes ((((class color) (background light)) (:background "#333333")))))
 
 (put 'downcase-region 'disabled nil)
