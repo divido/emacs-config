49db036afa5f3567962fa005d0ec9190e09db76a
David Diederich <david.diederich@gmail.com>
2019-08-20 17:47:18 -0400

    Increase font size

--- emacs-init-script.el
+++ emacs-init-script.el
@@ -338,7 +338,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 80 :family fontFamily))))
+ '(default ((t (:height 120 :family fontFamily))))
  '(semantic-decoration-on-unknown-includes ((((class color) (background light)) (:background "#333333")))))
 
 (put 'downcase-region 'disabled nil)
